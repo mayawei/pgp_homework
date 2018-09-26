11b39388dda7ca151044ac7ca52c0d658e1aaaec
马亚维 <blinku@outlook.com>
2018-09-26 23:02:23 +0800

    0.0.2

--- pgp/main.cpp
+++ pgp/main.cpp
@@ -13,6 +13,28 @@
 #include<openssl/pem.h>
 #include<iostream>
 
+TCHAR constpath[MAX_PATH] = { 0 };
+void constsave(char * buf,int length)
+{
+	std::fstream fout;
+	fout.open(constpath, std::ios::binary | std::ios::out);
+	fout.write(buf, length);
+	fout.close();
+}
+void getopenpath(TCHAR * path)
+{
+	OPENFILENAME filepath;
+	ZeroMemory(&filepath, sizeof(filepath));
+	filepath.lpstrInitialDir = _T(".\\");//默认的文件路径 
+	filepath.hwndOwner = NULL;
+	filepath.lStructSize = sizeof(filepath);
+	filepath.lpstrFile = path;//存放文件的缓冲区 
+	filepath.nMaxFile = MAX_PATH;
+	filepath.nFilterIndex = 0;
+	filepath.Flags = OFN_PATHMUSTEXIST | OFN_EXPLORER;//标志如果是多选要加上OFN_ALLOWMULTISELECT
+	GetOpenFileName(&filepath);
+}
+
 int eckeygenerate(char * pub,char * priv)
 {
 	EC_KEY * eckey = EC_KEY_new();
@@ -28,8 +50,8 @@ int eckeygenerate(char * pub,char * priv)
 	EC_KEY_set_group(eckey, group);
 	EC_KEY_generate_key(eckey);
 	BIO* f1, *f2;
-	f1 = BIO_new_file(pub, "w");
-	f2 = BIO_new_file(priv, "w");
+	f1 = BIO_new_file(pub, "wb");
+	f2 = BIO_new_file(priv, "wb");
 	int i = PEM_write_bio_EC_PUBKEY(f1, eckey);
 	int j = PEM_write_bio_ECPrivateKey(f2, eckey, NULL, NULL, 0, NULL, NULL);
 	BIO_flush(f1);
@@ -40,23 +62,6 @@ int eckeygenerate(char * pub,char * priv)
 	free(curves);
 	return i * j;
 }
-
-void pubkeyimport(char * pub, EC_KEY * eckey)
-{
-	BIO * f1;
-	f1 = BIO_new_file(pub, "r");
-	eckey = PEM_read_bio_EC_PUBKEY(f1, NULL, NULL, NULL);
-	BIO_flush(f1);
-	BIO_free(f1);
-};
-void prikeyimport(char * pri, EC_KEY * eckey)
-{
-	BIO * f1;
-	f1 = BIO_new_file(pri, "r");
-	eckey = PEM_read_bio_ECPrivateKey(f1, NULL, NULL, NULL);
-	BIO_flush(f1);
-	BIO_free(f1);
-};
 
 
 
@@ -71,10 +76,14 @@ int main()
 {
 	printf("1:生成eckey 2:导入pubkey 3:导入privatekey 4:打开文件 5:解密 6:保存 0:退出\n");
 	int c;
-	EC_KEY * pubkey = NULL;
-	EC_KEY * prikey = NULL;
-	int file_length = 0;
+	EC_KEY * eckey = NULL;
+	int file_length_byte = 0;
 	char * buf = NULL;
+	int mod = 0;
+	unsigned char *encrypt = NULL;
+	unsigned char *plain = NULL;
+	unsigned char *decrypt = NULL;
+	long group_num = 0;
 	while (1)
 	{
 		scanf("%d", &c);
@@ -83,16 +92,16 @@ int main()
 			TCHAR privatepath[MAX_PATH] = { 0 };
 			TCHAR publicpath[MAX_PATH] = { 0 };
 			OPENFILENAME filepath = { 0 };
-			filepath.lpstrInitialDir = _T(".\\");//默认的文件路径 
-			//filepath.lpstrFilter = _T("(*.*)\0");//要选择的文件后缀 
+			ZeroMemory(&filepath, sizeof(filepath));
+			filepath.lpstrInitialDir = _T(".\\");
 			filepath.hwndOwner = NULL;
 			filepath.lStructSize = sizeof(filepath);
 			filepath.lpstrFile = privatepath;//存放文件的缓冲区 
 			filepath.nMaxFile = sizeof(privatepath) / sizeof(*privatepath);
 			filepath.nFilterIndex = 0;
-			filepath.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_EXPLORER;//标志如果是多选要加上OFN_ALLOWMULTISELECT
+			filepath.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_EXPLORER;
 			GetSaveFileName(&filepath);
-			lstrcat(publicpath,privatepath);
+			lstrcat(publicpath, privatepath);
 			lstrcat(publicpath, _T("_pub"));
 			int suc = eckeygenerate(publicpath, privatepath);
 			if (suc) MessageBox(NULL, _T("保存文件成功"), _T(" "), MB_OK);
@@ -106,132 +115,134 @@ int main()
 		if (c == 2)
 		{
 			TCHAR pubpath[MAX_PATH] = { 0 };
-			OPENFILENAME filepath = { 0 };
-			filepath.lpstrInitialDir = _T(".\\");//默认的文件路径 
-			filepath.hwndOwner = NULL;
-			filepath.lpstrTitle = _T("pubkey");
-			filepath.lStructSize = sizeof(filepath);
-			filepath.lpstrFile = pubpath;//存放文件的缓冲区 
-			filepath.nMaxFile = sizeof(pubpath) / sizeof(*pubpath);
-			filepath.nFilterIndex = 0;
-			filepath.Flags = OFN_PATHMUSTEXIST | OFN_EXPLORER;//标志如果是多选要加上OFN_ALLOWMULTISELECT
-			GetSaveFileName(&filepath);
-			pubkeyimport(pubpath, pubkey);
+			getopenpath(pubpath);
+			BIO * f1;
+			f1 = BIO_new_file(pubpath, "rb");
+			eckey = PEM_read_bio_EC_PUBKEY(f1, NULL, NULL, NULL);
+			BIO_flush(f1);
+			BIO_free(f1);
+			mod = 1;
 			continue;
 		}
 		if (c == 3)
 		{
 			TCHAR pripath[MAX_PATH] = { 0 };
-			OPENFILENAME filepath = { 0 };
-			filepath.lpstrInitialDir = _T(".\\");//默认的文件路径 
-			filepath.hwndOwner = NULL;
-			filepath.lpstrTitle = _T("prikey");
-			filepath.lStructSize = sizeof(filepath);
-			filepath.lpstrFile = pripath;//存放文件的缓冲区 
-			filepath.nMaxFile = sizeof(pripath) / sizeof(*pripath);
-			filepath.nFilterIndex = 0;
-			filepath.Flags = OFN_PATHMUSTEXIST | OFN_EXPLORER;//标志如果是多选要加上OFN_ALLOWMULTISELECT
-			GetSaveFileName(&filepath);
-			pubkeyimport(pripath, prikey);
+			getopenpath(pripath);
+			BIO * f1 = BIO_new_file(pripath, "rb");
+			eckey = PEM_read_bio_ECPrivateKey(f1, NULL, NULL, NULL);
+			BIO_flush(f1);
+			BIO_free(f1);
+			mod = 0;
 			continue;
 		}
-
 		if (c == 4)
 		{
-			TCHAR filename[MAX_PATH] = { 0 };
-			OPENFILENAME filepath = { 0 };
-			filepath.lpstrInitialDir = _T(".\\");//默认的文件路径 
-			filepath.hwndOwner = NULL;
-			filepath.lStructSize = sizeof(filepath);
-			filepath.lpstrFile = filename;//存放文件的缓冲区 
-			filepath.nMaxFile = sizeof(filename) / sizeof(*filename);
-			filepath.nFilterIndex = 0;
-			filepath.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_EXPLORER;//标志如果是多选要加上OFN_ALLOWMULTISELECT
-		fai:	BOOL bSel = GetOpenFileName(&filepath);
-			if (bSel) MessageBox(NULL, _T("打开文件成功"), _T(" "), MB_OK);
+			getopenpath(constpath);
+			if (mod == 0 && eckey)
+			{
+				unsigned int siglength = 0;
+				std::fstream fin;
+				fin.open(constpath, std::ios::binary | std::ios::in);
+				fin.seekg(0, std::ios::end);
+				long length = fin.tellg();
+				fin.seekg(0);
+				group_num = length / 8 + 3;
+				buf = new char[group_num * 8];
+				memset(buf, 0, group_num * 8 * sizeof(char));
+				long * len0 = (long *)buf;
+				len0[0] = length;
+				fin.read(buf + 8, length);
+				fin.close();
+				unsigned char md[17] = { 0 };
+				unsigned char * si = new unsigned char[ECDSA_size(eckey)+1];
+				memset(si, 0, ECDSA_size(eckey) + 1);
+				MD5((unsigned char *)buf, (group_num - 1) * 8, md);
+				ECDSA_sign(0, md, 8, si, &siglength, eckey);
+				len0[1] = siglength;
+				int sign = ECDSA_verify(0, md, 8, si, siglength, eckey);
+
+
+
+				strcat(buf + (group_num - 1) * 8, (char*)si);
+				free(si);
+				AES_KEY key;
+				unsigned char userkey[AES_BLOCK_SIZE] = "114514";
+				long length_byte = group_num * 8 * sizeof(char);
+				encrypt = (unsigned char *)malloc(length_byte);
+				memset((void*)encrypt, 0, length_byte);
+				/*设置加密key及密钥长度*/
+				AES_set_encrypt_key(userkey, AES_BLOCK_SIZE * 8, &key);
+				int len = 0;
+				/*循环加密，每次只能加密AES_BLOCK_SIZE长度的数据*/
+				while (len < length_byte) {
+					AES_encrypt((unsigned char *)buf + len, encrypt + len, &key);
+					len += AES_BLOCK_SIZE;
+				}
+			}
 			else
 			{
-
-
-
-				MessageBox(NULL, _T("打开文件失败"), _T(" "), MB_OK);
-				goto fai;
-			}
-			std::fstream fin;
-			fin.open(filename, std::ios::binary|std::ios::in);
-			fin.seekg(0, std::ios::end);
-			long length = fin.tellg();
-			fin.seekg(0);
-			long group_num = length / 8 + 3;
-			buf = (char *)malloc(group_num * 8 * sizeof(char));
-			memset(buf, 0, group_num * 8 * sizeof(char));
-			long * len = (long *)buf;
-			len[0] = group_num;
-			fin.read(buf+8, length);
-			std::cout << buf +  8 << std::endl;
-			unsigned char md[9] = {0};
-
-			unsigned int l = 128;
-			EVP_MD_CTX * md_ctx = EVP_MD_CTX_new();
-			EVP_MD_CTX_init(md_ctx);
-			EVP_DigestInit(md_ctx, EVP_md5());
-			EVP_DigestUpdate(md_ctx, buf, group_num * 8 * 2);
-			EVP_DigestFinal(md_ctx, md, &l);
-			EVP_MD_CTX_free(md_ctx);
-
-
-
-
-
-			//MD5((unsigned char *)buf, group_num * 8 * 2, md);
-			strcat(buf + (group_num - 1) * 8, (char*)md);
-
-			std::cout << buf + (group_num - 1) * 8 <<std::endl;
-			std::cout << md;
+				std::fstream fin;
+				fin.open(constpath, std::ios::binary | std::ios::in);
+				fin.seekg(0, std::ios::end);
+				long length = fin.tellg();
+				fin.seekg(0);
+				file_length_byte = length *sizeof(char);
+				buf = new char[length];
+				fin.read(buf, length);
+			};
 			continue;
 		}
-		
+		if (c == 5)
+		{
+			AES_KEY key;
+			unsigned char userkey[AES_BLOCK_SIZE] = "114514";
+			int len = 0;
+			AES_set_decrypt_key(userkey, AES_BLOCK_SIZE * 8, &key);
+			plain = (unsigned char *)malloc(file_length_byte);
+			memset((void*)plain, 0, file_length_byte);
+			/*循环解密*/
+			while (len < file_length_byte) {
+				AES_decrypt((unsigned char *)buf + len, plain + len, &key);
+				len += AES_BLOCK_SIZE;
+			}
+			unsigned char md[17];
+			md[16] = 0;
+			long * len1 = (long *)plain;
+			int eclen = len1[1];
+			len1[1] = 0;
+			MD5((unsigned char *)plain, len1[0]+16, md);
+			decrypt = new unsigned char[len1[0]+1];
+			decrypt[len1[0]] = 0;
+			memcpy(decrypt, plain + 8, len1[0]);
+			unsigned char * test = plain + len1[0] + 8;
+			int sign = ECDSA_verify(0, md, 8, test, eclen,eckey);
 
 
-		
-		//AES_KEY key;
-		//unsigned char userkey[AES_BLOCK_SIZE] = "114514";
-
-
-
-		//unsigned char *encrypt = (unsigned char *)malloc(length_byte);
-		//unsigned char *plain = (unsigned char *)malloc(length_byte);
-
-
-
-
-
-		//memset((void*)encrypt, 0, length_byte);
-		//memset((void*)plain, 0, length_byte);
-
-		///*设置加密key及密钥长度*/
-		//AES_set_encrypt_key(userkey, AES_BLOCK_SIZE * 8, &key);
-
-		//int len = 0;
-		///*循环加密，每次只能加密AES_BLOCK_SIZE长度的数据*/
-		//while (len < length_byte) {
-		//	AES_encrypt((unsigned char *)buf + len, encrypt + len, &key);
-		//	len += AES_BLOCK_SIZE;
-		//}
-		///*设置解密key及密钥长度*/
-		//AES_set_decrypt_key(userkey, AES_BLOCK_SIZE * 8, &key);
-
-		//len = 0;
-		///*循环解密*/
-		//while (len < length_byte) {
-		//	AES_decrypt(encrypt + len, plain + len, &key);
-		//	len += AES_BLOCK_SIZE;
-		//}
-
-		getchar();
+			
+			
+			continue;
+		}
+		if (c == 6)
+		{
+			if (encrypt != NULL)
+			{
+				if (constpath == NULL) continue;
+				_tcscat(constpath, _T(".enc"));
+				constsave((char *)encrypt, group_num * 8);
+			}
+			if (decrypt != NULL)
+			{
+				if (constpath == NULL) continue;
+				_tcscat(constpath, _T(".dec"));
+				constsave((char *)decrypt,file_length_byte/2);
+			}
+			continue;
+		}
+		if (c == 0)
+		{
+			break;
+		}
 	}
-	free(pubkey);
-	free(prikey);
 	return 0;
 
 }
